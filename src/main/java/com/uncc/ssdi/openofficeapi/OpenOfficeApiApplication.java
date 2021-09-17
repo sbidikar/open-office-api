85da391ebc7dbb68cbde27f58f932611c7543bfa
Siddharth Bidikar <siddharthbidikar@Siddharths-MacBook-Pro.local>
2021-09-16 21:00:02 -0400

    Initial Commit

---
+++ src/main/java/com/uncc/ssdi/openofficeapi/OpenOfficeApiApplication.java
@@ -0,0 +1,13 @@
+package com.uncc.ssdi.openofficeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenOfficeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenOfficeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uncc/ssdi/openofficeapi/OpenOfficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncc.ssdi.openofficeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenOfficeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
