e72706915bb73d13de0da48835252b6d619e2260
Siddharth Bidikar <siddharthbidikar@Siddharths-MacBook-Pro.local>
2021-09-26 12:13:18 -0400

    added cross origin

--- src/main/java/com/uncc/ssdi/openofficeapi/controller/OpenOfficeController.java
+++ src/main/java/com/uncc/ssdi/openofficeapi/controller/OpenOfficeController.java
@@ -3,6 +3,7 @@ package com.uncc.ssdi.openofficeapi.controller;
 import com.uncc.ssdi.openofficeapi.entity.OfficeFloorMap;
 import com.uncc.ssdi.openofficeapi.service.OpenOfficeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class OpenOfficeController {
 
     @Autowired
