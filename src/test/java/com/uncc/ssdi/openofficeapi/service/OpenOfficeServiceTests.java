48b6d9c2b1abd4f9f1b63afabf1ffe32a6ed3f85
NealGragg <ngragg@uncc.edu>
2021-11-09 21:07:55 -0500

    unit tests added

--- src/test/java/com/uncc/ssdi/openofficeapi/service/OpenOfficeServiceTests.java
+++ src/test/java/com/uncc/ssdi/openofficeapi/service/OpenOfficeServiceTests.java
@@ -1,7 +1,11 @@
 package com.uncc.ssdi.openofficeapi.service;
 
 import com.uncc.ssdi.openofficeapi.entity.OfficeFloorMap;
+import com.uncc.ssdi.openofficeapi.entity.ReservedDesk;
+import com.uncc.ssdi.openofficeapi.model.SearchRequest;
+import com.uncc.ssdi.openofficeapi.model.UserDetail;
 import com.uncc.ssdi.openofficeapi.repository.OfficeFloorMapRepository;
+import com.uncc.ssdi.openofficeapi.repository.ReservedDeskRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -21,10 +26,11 @@ public class OpenOfficeServiceTests {
 
     @Mock
     OfficeFloorMapRepository officeFloorMapRepository;
+    @Mock
+    ReservedDeskRepository reservedDeskRepository;
 
     @InjectMocks
     OpenOfficeService openOfficeService;
-
 
     @Test
     public void testFindAll() {
@@ -41,5 +47,33 @@ public class OpenOfficeServiceTests {
         List<OfficeFloorMap> officeFloorMapsResult = openOfficeService.findAll();
         verify(officeFloorMapRepository).findAll();
         assertEquals(officeFloorMapsMock,officeFloorMapsResult);
+    }
+
+    @Test
+    public void testSave() {
+        ReservedDesk testRD = new ReservedDesk();
+        String saveOutput = openOfficeService.save(testRD);
+        assertEquals(saveOutput,"Desk reserved successfully!");
+    }
+
+    @Test
+    public void testSearch() {
+        OfficeFloorMap officeFloorMap = new OfficeFloorMap();
+        officeFloorMap.setId(new Long(1));
+        officeFloorMap.setLocation("Test Location");
+        officeFloorMap.setBuilding("Test Building");
+        officeFloorMap.setFloor("Test Floor");
+        officeFloorMap.setDesk("Test Desk");
+        List<OfficeFloorMap> officeFloorMapsMock = new ArrayList<>();
+        officeFloorMapsMock.add(officeFloorMap);
+        when(officeFloorMapRepository.findAll()).thenReturn(officeFloorMapsMock);
+        SearchRequest testSR = new SearchRequest();
+        testSR.setBuilding("Test Building");
+        testSR.setFloor("Test Floor");
+        Date date = Date.valueOf("2021-10-13");
+        testSR.setReserveDate(date);
+        List<OfficeFloorMap> searchOutput = openOfficeService.search(testSR);
+        OfficeFloorMap result = searchOutput.get(0);
+        assertEquals(result.getBuilding(), testSR.getBuilding());
     }
 }
